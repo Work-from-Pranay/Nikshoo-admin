c6f188683b0e25049dc62a0b62e014500b6f8bd6
Work-from-Pranay <mishrapranay21@gmail.com>
2024-10-21 20:41:24 +0530

    admin-img

--- client/pages/Admin-image.jsx
+++ client/pages/Admin-image.jsx
@@ -2,14 +2,11 @@ import React, { useEffect, useState } from "react";
 import './Admin.css'; // Importing the CSS file
 import { FaSpinner } from 'react-icons/fa';
 
-
 const AdminImage = () => {
     const [images, setImages] = useState([]); // State for images to display
     const [selectedFiles, setSelectedFiles] = useState([]); // State for selected files
     const [isUploading, setIsUploading] = useState(false); // State to track uploading status
     const [deletingFile, setDeletingFile] = useState(null); // State to track the file being deleted
-    const [editingFileName, setEditingFileName] = useState(null); // State for the file being edited
-    const [newFileName, setNewFileName] = useState(''); // State for new file name input
 
     const uid = 'EyrEUxvYnVZueLMjvX3LOX7RHVb2'; // Your uid
 
@@ -65,8 +62,41 @@ const AdminImage = () => {
         setIsUploading(false); // Stop uploading
     };
 
+
+    const handleEditImage = async (file, imageName) => {
+        if (!file) return;
+
+        setIsUploading(true); // Start uploading
+
+        const formData = new FormData();
+        formData.append('image', file); // Append the new file
+        formData.append('rename', imageName); // Append the image name to rename
+
+        try {
+            const response = await fetch(`https://nikshoo-backend.vercel.app/admin/editImage?uid=${uid}`, {
+                method: 'PUT',
+                body: formData,
+            });
+
+            if (response.ok) {
+                console.log('Image replaced successfully');
+                fetchImages(); // Refresh the image list after upload
+            } else {
+                console.error('Error editing image:', response.statusText);
+            }
+        } catch (error) {
+            console.error('Error editing image:', error);
+        }
+
+        setIsUploading(false); // Stop uploading
+    };
+
     // Function to delete an image
     const handleDeleteImage = async (fileName) => {
+        // Ask user for confirmation before proceeding with deletion
+        const userConfirmed = window.confirm("Are you sure you want to delete this image?");
+        if (!userConfirmed) return; // Exit if user cancels
+
         setDeletingFile(fileName); // Set the file being deleted
 
         try {
@@ -89,37 +119,6 @@ const AdminImage = () => {
         }
 
         setDeletingFile(null); // Reset deletion status
-    };
-
-    // Function to submit the new name
-    const handleSubmitNewName = async (e) => {
-        e.preventDefault();
-        if (!newFileName) return; // Do not submit if the new name is empty
-
-        try {
-            const response = await fetch('https://nikshoo-backend.vercel.app/admin/editImage', {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    imageName: editingFileName, // Current image name
-                    newName: newFileName // New name input by the user
-                }),
-            });
-
-            if (response.ok) {
-                console.log('Image name updated successfully');
-                fetchImages(); // Fetch images again to update the UI
-            } else {
-                console.error('Error updating image name:', response.statusText);
-            }
-        } catch (error) {
-            console.error('Error updating image name:', error);
-        }
-
-        setEditingFileName(null); // Reset editing status
-        setNewFileName(''); // Clear the input field
     };
 
     return (
@@ -160,20 +159,22 @@ const AdminImage = () => {
                 </div>
 
                 {/* Upload Button */}
-                <button
-                    onClick={handleUploadImages}
-                    style={{
-                        padding: '10px',
-                        borderRadius: '5px',
-                        backgroundColor: '#09655b',
-                        color: 'white',
-                        border: 'none',
-                        cursor: 'pointer',
-                        marginTop: '10px',
-                    }}
-                >
-                    {isUploading ? 'Uploading...' : 'Upload'}
-                </button>
+                {selectedFiles.length > 0 && (
+                    <button
+                        onClick={handleUploadImages}
+                        style={{
+                            padding: '10px',
+                            borderRadius: '5px',
+                            backgroundColor: '#09655b',
+                            color: 'white',
+                            border: 'none',
+                            cursor: 'pointer',
+                            marginTop: '10px',
+                        }}
+                    >
+                        {isUploading ? 'Uploading...' : 'Upload'}
+                    </button>
+                )}
             </div>
 
             <h2>Uploaded Images</h2>
@@ -181,26 +182,42 @@ const AdminImage = () => {
             <div className="image-cards-container images" style={{ maxHeight: '400px', overflowY: 'auto' }}>
                 <div className="image-cards" style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
                     {images.length > 0 ? (
-                        images.map((image, index) => (
-                            <div key={index} className="image-card" style={{ position: 'relative' }}>
-                                <img src={image.imageUrl} alt={`Uploaded ${index}`} style={{ width: '100%', borderRadius: '5px' }} />
-                                <div className="overlay">
-                                    <span className="image-name">{image.fileName}</span>
-                                    <button
-                                        onClick={() => {
-                                            setEditingFileName(image.fileName);
-                                            setNewFileName(''); // Reset input on edit
-                                        }}
-                                        className="edit"
-                                    >
-                                        Edit Name
-                                    </button>
-                                    <button onClick={() => handleDeleteImage(image.fileName)} disabled={deletingFile === image.fileName}>
-                                        {deletingFile === image.fileName ? 'Deleting...' : 'Delete'}
-                                    </button>
+                        images.map((image, index) => {
+                            const imageheading = image.fileName.split('-').pop().split('.')[0];
+                            return (
+                                <div key={index} className="image-card" style={{ position: 'relative' }}>
+                                    <img src={image.imageUrl} alt={`Uploaded ${index}`} style={{ width: '100%', borderRadius: '5px' }} />
+
+                                    <div className="overlay">
+                                        <span className="image-name">{imageheading}</span>
+
+                                        <input
+                                            type="file"
+                                            id={`file-input-${index}`}
+                                            style={{ display: 'none' }}
+                                            onChange={(e) => {
+                                                handleEditImage(e.target.files[0], image.fileName);
+                                            }}
+                                        />
+
+                                        {/* Button to trigger file input */}
+                                        <button
+                                            className="edit"
+                                            onClick={() => document.getElementById(`file-input-${index}`).click()}
+                                        >
+                                            Edit Image
+                                        </button>
+
+                                        <button
+                                            onClick={() => handleDeleteImage(image.fileName)}
+                                            disabled={deletingFile === image.fileName}
+                                        >
+                                            {deletingFile === image.fileName ? 'Deleting...' : 'Delete'}
+                                        </button>
+                                    </div>
                                 </div>
-                            </div>
-                        ))
+                            );
+                        })
                     ) : (
                         <div className="loading-container">
                             <FaSpinner className="loading-icon imgone" />
@@ -208,26 +225,6 @@ const AdminImage = () => {
                     )}
                 </div>
             </div>
-
-            {/* Popup Form for Editing Image Name */}
-            {editingFileName && (
-                <div className="popup-form">
-                    <div className="form-container">
-                        <h3>Edit Image Name</h3>
-                        <form onSubmit={handleSubmitNewName}>
-                            <input
-                                type="text"
-                                placeholder="New Image Name"
-                                value={newFileName}
-                                onChange={(e) => setNewFileName(e.target.value)}
-                                required
-                            />
-                            <button type="submit">Submit</button>
-                            <button type="button" onClick={() => setEditingFileName(null)}>Cancel</button>
-                        </form>
-                    </div>
-                </div>
-            )}
         </div>
     );
 };
